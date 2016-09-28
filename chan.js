2fd839cf97a545839b91dd91057da4563ee80228
TJBK <martin@desu.tech>
2016-09-28 18:41:42 +0100

    Ok now it should work without error
    I hope

--- chan.js
+++ chan.js
@@ -12,75 +12,75 @@ mong.connect("mongodb://localhost:27017");
 var db = mong.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log("Opened connection!");
+    console.log("Opened connection!");
 });
 
 //This the basic text based post schema. There will be one that includes images in later builds!
 var postSchema = mong.Schema({
-  title: String,
-  message: String,
-  postNum: Number
+    title: String,
+    message: String,
+    postNum: Number
 });
 
 var post = mong.model('post', postSchema);
 
 app.use(express.static(__dirname + '/front-end'));
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/front-end/index.html");
+    res.sendFile(__dirname + "/front-end/index.html");
 });
 
 io.on("connection", (socket) => {
-  console.log("Connected to the server");
+    console.log("Connected to the server");
 
-  displayPosts(socket);
+    displayPosts(socket);
 
-  socket.on("disconnect", () => {
-    console.log("Disconnected from server");
-  });
-
-  //When the user requests to post something, this will be called!
-  //This function essentially saves the post to the database, and then in return will display the post
-  socket.on("requestPost", (data) => {
-    var pst = new post({title: data.title, message: data.message, postNum: gpn});
-    pst.save((err, pst) => {
-      if(err) return console.error(err);
-
-      console.log("Saved post: " + gpn);
-      gpn += 1;
+    socket.on("disconnect", () => {
+        console.log("Disconnected from server");
     });
-  });
+
+    //When the user requests to post something, this will be called!
+    //This function essentially saves the post to the database, and then in return will display the post
+    socket.on("requestPost", (data) => {
+        var pst = new post({ title: data.title, message: data.message, postNum: gpn });
+        pst.save((err, pst) => {
+            if (err) return console.error(err);
+
+            console.log("Saved post: " + gpn);
+            gpn += 1;
+        });
+    });
 });
 
 function displayPosts(sock) {
-  post.find((err, posts) => {
-    if(err) return console.error(err);
-    sock.emit("showPosts", posts);
-  });
+    post.find((err, posts) => {
+        if (err) return console.error(err);
+        sock.emit("showPosts", posts);
+    });
 }
 
 http.listen(3001, () => {
-  console.log("Listening on 3001");
+    console.log("Listening on 3001");
 });
 
 
 //Closes
-process.stdin.resume();//so the program will not close instantly
+process.stdin.resume(); //so the program will not close instantly
 
 function exitHandler(options, err) {
     if (options.cleanup) console.log('clean');
     if (err) console.log(err.stack);
     if (options.exit) process.exit();
     // Clear the DB
-    post.remove({}, (err) =>
-	    if(err) return console.error(err);
+    post.remove({}, (err) => {
+        if (err) return console.error(err);
     });
 }
 
 //do something when app is closing
-process.on('exit', exitHandler.bind(null,{cleanup:true}));
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
 
 //catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
 
 //catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
