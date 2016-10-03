dd16ab944b020e13cebff49fdd216d75cfbf2486
Sloansta <Sloan.boyce@gmail.com>
2016-10-03 17:56:45 -0400

    Image uploading still isn't working correctly, hopefully in the next commit, we can change that!

--- front-end/main.js
+++ front-end/main.js
@@ -4,9 +4,15 @@ var submit = $("#submit");
 //When a user connects to the server, this will display them the current posts
 socket.on("showPosts", (data) => {
     $('#header-title').css("color", randomColor());
+    var title = "",
+        message = "";
     for (var i in data) {
-        var title = '<div id="' + data[i].postNum + '"><h3 class="inline">' + data[i].title + ' [post no.' + data[i].postNum + ']</h3><button type="submit" id="b_' + data[i].postNum + '" onclick="postComment(' + data[i].postNum + ')" class="inline button-comment">Reply</button> </div>',
-            message = '<div class="message"><p>' + data[i].message + '</p></div>';
+        if(data[i].img.data != null)
+          title = '<div id="' + data[i].postNum + '"> <img src="data:image/jpeg;base64,'+data.img.data+'""/><h3 class="inline">' + data[i].title + ' [post no.' + data[i].postNum + ']</h3><button type="submit" id="b_' + data[i].postNum + '" onclick="postComment(' + data[i].postNum + ')" class="inline button-comment">Reply</button> </div>';
+        else
+          title = '<div id="' + data[i].postNum + '"><h3 class="inline">' + data[i].title + ' [post no.' + data[i].postNum + ']</h3><button type="submit" id="b_' + data[i].postNum + '" onclick="postComment(' + data[i].postNum + ')" class="inline button-comment">Reply</button> </div>',
+
+        message = '<div class="message"><p>' + data[i].message + '</p></div>';
         $(".postcontainer").append("<div class=\"post\">" + title + message + "</div><hr/>");
 
         if (data[i].comments.length > 0) {
@@ -20,20 +26,19 @@ socket.on("showPosts", (data) => {
     }
 });
 
-//Event listener for when the user wants to upload an image and or file
-$("#fileinput").on('change', () => {
-  //Next commit this will handle the image uploading and sending the data to the server to be saved!!!!
-});
 
 //Submits a post to the database.
 function submitPost() {
     var title = $("#title").val(),
-        message = $("#message").val();
-
-    if (title != null && title != "" && message != null && message != "") {
+        message = $("#message").val(),
+        img = $("#fileinput")[0].files[0];
+    console.log(img);
+    if (title != null && title != "" && message != null && message != "" && img != null) {
+        console.log("called");
         socket.emit("requestPost", {
             title: title,
-            message: message
+            message: message,
+            img: img
         });
     } else
         alert("Boxes cannot be empty and or null!!!!");
